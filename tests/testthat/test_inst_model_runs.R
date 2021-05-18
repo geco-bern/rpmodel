794c7bf0084a65e0429bfeb9ea77e4c302fc125e
Koen Hufkens <koen.hufkens@gmail.com>
2021-05-18 17:32:41 +0200

    inst model call

--- tests/testthat/test_inst_model_runs.R
+++ tests/testthat/test_inst_model_runs.R
@@ -4,23 +4,6 @@ test_that("inst model run",{
   skip_on_cran()
   
   # no atmospheric pressure given
-  expect_warning(inst_rpmodel( 
-    tc             = 20,
-    vpd            = 1000,
-    co2            = 400,
-    fapar          = 1,
-    ppfd           = 300,
-    elv            = 0,
-    kphio          = 0.049977,
-    beta           = 146,
-    c4             = FALSE,
-    method_optci   = "prentice14",
-    method_jmaxlim = "none",
-    do_ftemp_kphio = FALSE,
-    do_soilmstress = FALSE,
-    verbose        = TRUE
-  ))
-  
   out_pmodel <- rpmodel( 
     tc             = 20,
     vpd            = 1000,
@@ -39,6 +22,17 @@ test_that("inst model run",{
     verbose        = TRUE
   )
   
+  # inst call
+  inst_out <- inst_rpmodel(
+    out_pmodel,
+    tc = 20,
+    vpd = 1000,
+    co2 = 400,
+    fapar = 1,
+    patm = 1024,
+    ppfd = 300,
+    kphio = 1)
+  
   # output must be a list (no atmosphere warning)
-  expect_type(out_pmodel, "list")
+  expect_type(inst_out, "list")
 })
